86c6fe7c527bce284776646883c5aa4c6102bc1d
Luciferz2012 <luciferz2012@gmail.com>
2016-02-06 18:20:16 +0800

    add support for fn key

---
+++ ahk_map.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+__author__ = 'Luciferz2012'
+
+fn = {
+    'F1': 'Volume_Mute',
+    'F2': 'Volume_Down',
+    'F3': 'Volume_Up',
+    'F4': 'Media_Prev',
+    'F5': 'Media_Play_Pause',
+    'F6': 'Media_Next',
+    'F7': 'Browser_Back',
+    'F8': 'Browser_Home',
+    'F9': 'AppsKey',
+    'F10': 'RAlt',
+    'F11': 'PrintScreen',
+    'F12': 'RCtrl'
+}
+
+if __name__ == '__main__':
+    with open('fn.ahk', 'w') as f:
+        lines = []
+        for i in range(1, 10):
+            key = 'F%s' % i
+            lines.append('{key}::{value}'.format(key=fn[key], value=key))
+            lines.append('^{key}::^{value}'.format(key=fn[key], value=key))
+            lines.append('!{key}::!{value}'.format(key=fn[key], value=key))
+            lines.append('^!{key}::^!{value}'.format(key=fn[key], value=key))
+        f.writelines('\n'.join(lines))
